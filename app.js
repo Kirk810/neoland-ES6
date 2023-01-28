5861fe9065983340277c41475e13fde93f9717f3
Adrián <123007421+Kirk810@users.noreply.github.com>
2023-01-28 01:18:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -89,3 +89,39 @@ const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
 const color = [...colors];
 color.splice(1, 1);
 console.log(color);
+
+//Iteracion #4: Map
+/*4.1 Dado el siguiente array, devuelve un array con sus nombres 
+utilizando .map().*/
+const users = [
+	{id: 1, name: 'Abel'},
+	{id:2, name: 'Julia'},
+	{id:3, name: 'Pedro'},
+	{id:4, name: 'Amanda'}
+];
+const names = users.map(user => user.name);
+console.log(names);
+
+/*4.2 Dado el siguiente array, devuelve una lista que contenga los valores 
+de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que 
+empiece por 'A'.*/
+const users2 = [
+	{id: 1, name: 'Abel'},
+	{id:2, name: 'Julia'},
+	{id:3, name: 'Pedro'},
+	{id:4, name: 'Amanda'}
+];
+const names2 = users2.map(user => user.name.startsWith('A') ? "Anacleto": user.name)
+console.log(names2);
+
+/*4.3 Dado el siguiente array, devuelve una lista que contenga los valores 
+de la propiedad .name y añade al valor de .name el string ' (Visitado)' 
+cuando el valor de la propiedad isVisited = true.*/
+const cities = [
+	{isVisited:true, name: 'Tokyo'}, 
+	{isVisited:false, name: 'Madagascar'},
+	{isVisited:true, name: 'Amsterdam'}, 
+	{isVisited:false, name: 'Seul'}
+];
+const cities2 = cities.map(value => (value.isVisited === true)? value.name = '(Visitado)' : value.name);
+console.log(cities);
